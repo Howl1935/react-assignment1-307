d7314b1aebc06d7fa261f49ee6129b54d00a672d
Howl1935 <travisphawley@gmail.com>
2021-09-25 13:28:29 -0700

    At stage 4 of assignment 2

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from './Table'
-const characters = [
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
     {
       name: 'Charlie',
       job: 'Janitor',
     },
-    {
+      {
       name: 'Mac',
       job: 'Bouncer',
     },
@@ -17,13 +19,26 @@ const characters = [
       name: 'Dennis',
       job: 'Bartender',
     },
-];
-function MyApp() {
-    return (
-      <div className="container">
-          <Table characterData={characters} />
-      </div>
-    );  
+    
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
+
+  return (
+  <div className="container">
+      <Table characterData={characters}  removeCharacter={removeOneCharacter} />
+  </div>
+);  
+
 }
 
 export default MyApp;
+
+
+
+
