32c368e9d61fbca6477af9335d0ca9455c5ef311
Howl1935 <travisphawley@gmail.com>
2021-09-27 13:46:33 -0700

    Changes up to step 4 API

--- backend/hello_world.py
+++ backend/hello_world.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import request
+
+
 app = Flask(__name__)
-
-
 
 users = { 
    'users_list' :
@@ -37,7 +38,29 @@ users = {
 
 
 
-@app.route('/users')
-def get_users():
-    return users
+
+
+# @app.route('/users')
+# def get_users():
+#    search_username = request.args.get('name') #accessing the value of parameter 'name'
+#    if search_username :
+#       subdict = {'users_list' : []}
+#       for user in users['users_list']:
+#          if user['name'] == search_username:
+#             subdict['users_list'].append(user)
+#       return subdict
+#    return users
+
+
+
+
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
+   return users
+
 
