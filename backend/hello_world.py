15c574e61877708472c9c751a3b430b714ace26b
Howl1935 <travisphawley@gmail.com>
2021-09-28 20:43:09 -0700

    Linking frontend and backend part 1

--- backend/hello_world.py
+++ backend/hello_world.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import request
-
+from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 users = { 
    'users_list' :
@@ -37,7 +39,9 @@ users = {
 
 
 
-
+@app.route('/')
+def get_user_list():
+   return users
 
 
 # @app.route('/users')
@@ -54,13 +58,46 @@ users = {
 
 
 
-@app.route('/users/<id>')
-def get_user(id):
-   if id :
+# @app.route('/users/<id>')
+# def get_user(id):
+#    if id :
+#       for user in users['users_list']:
+#         if user['id'] == id:
+#            return user
+#       return ({})
+#    return users
+
+
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+   if request.method == 'DELETE':
       for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
-   return users
-
-
+         if user['id'] == id:
+            users['users_list'].remove(user)
+      resp = jsonify(success=True)
+   return resp      
+      
+         
+  
--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios'
+
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetchAll().then( result => {
+      if(result)
+        setCharacters(result);
+    });
+  }, []);
+  
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:5000/users')
+      return response.data.users_list;
+    }catch(error){
+      //we're actually not catching errors... just logging to console
+      console.log(error)
+      return false
+    }
+  }
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
